6c76b3b8651a46d633f872aee4479c8ac5db942b
salmaashrmn <80728881+salmaashrmn@users.noreply.github.com>
2021-10-01 03:30:03 +0700

    Update GajiAgent.java

--- W3/GajiAgent.java
+++ W3/GajiAgent.java
@@ -11,27 +11,26 @@ import java.util.*;
 
 public class GajiAgent {
     public static void main(String[] args){
-        int penjualan;
-        long totalPenjualan, bonus, gaji, minus;
+        int penjualan, totalPenjualan, bonus, gaji, minus;
         
         Scanner keyboard = new Scanner (System.in);
         
         for(;;){
         penjualan = keyboard.nextInt();
         if(penjualan>=40){
-            totalPenjualan = (long) (50000*penjualan);
-            bonus = (long)(0.25*totalPenjualan);
+            totalPenjualan = 50000*penjualan;
+            bonus = (int) 0.25*totalPenjualan;
             gaji = 500000+bonus;
         }else if(penjualan>80){
-            totalPenjualan = (long) (50000*penjualan);
-            bonus = (long)(0.35*totalPenjualan);
+            totalPenjualan = 50000*penjualan;
+            bonus = (int) 0.35*totalPenjualan;
             gaji = 500000+bonus;
         }else if(penjualan<15){
             penjualan = 15-penjualan;
-            minus = (long)((0.15*50000)*penjualan);
+            minus = (int) (0.15*50000)*penjualan;
             gaji = 500000-minus;
         }else{
-            bonus = (long)((0.10*50000)*penjualan);
+            bonus = (int) (0.10*50000)*penjualan;
             gaji = 500000+bonus;
         }
         System.out.println(gaji);
