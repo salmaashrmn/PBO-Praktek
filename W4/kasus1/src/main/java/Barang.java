1e61d9a57cae8ef60a21ceafadb20b652ea64ce0
salmaashrmn <salma.aulia.tif20@polban.ac.id>
2021-10-08 05:41:42 +0700

    source code tugas week 4

---
+++ W4/kasus1/src/main/java/Barang.java
@@ -0,0 +1,20 @@
+public class Barang {
+    String kode_barang;
+    String nama_barang;
+    private int stok;
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok += stok;
+    }
+
+    public Barang(String kode, String nama, int stk) {
+        kode_barang = kode;
+        nama_barang = nama;
+        stok = stk;
+    }
+}
+
---
+++ W4/kasus2/src/main/java/Item.java
@@ -0,0 +1,10 @@
+public class Item {
+    private String name;
+    //private Item() {
+    //name = "Ipin";
+    //}
+    public Item(String name) {
+        this.name = "Ipin";
+        System.out.println(this.name);
+    }
+}
